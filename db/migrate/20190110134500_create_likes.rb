f5caed4c66a3090587dff2d8c47d1d3975b3fed0
koichi.sudo <koichisudo090807@gmail.com>
2019-01-14 11:42:02 +0000

    for heroku

--- db/migrate/20190110134500_create_likes.rb
+++ db/migrate/20190110134500_create_likes.rb
@@ -2,7 +2,7 @@ class CreateLikes < ActiveRecord::Migration[5.0]
   def change
     create_table :likes do |t|
       t.references :user, foreign_key: true
-      t.references :micropost, foreign_key: { to_table: :users }
+      t.references :micropost, foreign_key: true
 
       t.timestamps
       
